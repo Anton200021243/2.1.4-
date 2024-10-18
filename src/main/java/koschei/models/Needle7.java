3ad68dd6eb50045caff9bab602da17b48dd3bfda
Anton200021243 <mortal0011@gmail.com>
2024-10-18 20:33:30 +0300

    2.1.4 complete

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -5,12 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
-
     Deth8 deth;
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( ";
     }
 
     @Autowired
